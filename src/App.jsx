7e5423bd49951e2e820c5b1116d5b82f0702aebb
Dhiren507 <62496850+Dhiren507@users.noreply.github.com>
2025-06-03 15:23:43 +0530

    Minor Adjustment for Deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import TextField from '@mui/material/TextField'
-import './App.css'
 import Typography from '@mui/material/Typography'
 import Container from '@mui/material/Container'
 import Box from '@mui/material/Box'
